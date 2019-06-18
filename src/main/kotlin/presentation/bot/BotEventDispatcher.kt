634710e1407388000755e9385376026104dad2a5
inkiu <hik0115@gmail.com>
2019-06-18 15:02:25 +0900

    파일 저장 로직 추가

--- src/main/kotlin/presentation/bot/BotEventDispatcher.kt
+++ src/main/kotlin/presentation/bot/BotEventDispatcher.kt
@@ -1,19 +1,31 @@
 package presentation.bot
 
+import di.ImageFolder
 import domain.model.ChatContent
 import domain.model.PostChat
+import domain.usecase.RegisterFile
 import javax.inject.Inject
 
 class BotEventDispatcher @Inject constructor(
-    private val postChat: PostChat
+    private val postChat: PostChat,
+    private val registerFile: RegisterFile
 ) {
 
     private val botMap: MutableMap<RoomKey, Friday> = mutableMapOf()
 
     fun onChat(chat: ChatContent) {
         val roomKey = RoomKey(chat.teamIndex, chat.roomIndex)
-        val friday = botMap[roomKey] ?: Friday(postChat).apply { botMap[roomKey] = this }
+        val friday = botMap[roomKey] ?: createFriday(chat).apply { botMap[roomKey] = this }
         friday.onChatEvent(chat)
+    }
+
+    private fun createFriday(chat: ChatContent): Friday {
+        return Friday(
+            postChat,
+            registerFile,
+            chat.teamIndex,
+            chat.roomIndex
+        )
     }
 
     data class RoomKey(
--- src/main/kotlin/presentation/bot/Friday.kt
+++ src/main/kotlin/presentation/bot/Friday.kt
@@ -3,19 +3,75 @@ package presentation.bot
 import domain.model.ChatContent
 import domain.model.ChatEvent
 import domain.model.PostChat
+import domain.usecase.RegisterFile
 import kotlinx.coroutines.*
+import java.io.File
 import kotlin.coroutines.CoroutineContext
 
 class Friday(
-    private val postChat: PostChat
+    private val postChat: PostChat,
+    private val registerFile: RegisterFile,
+    private val teamIndex: Long,
+    private val roomIndex: Long
 ) : CoroutineScope {
+
+    sealed class State {
+        object IDLE: State()
+        class REGISTER(val key: String): State()
+    }
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Default + Job() + CoroutineExceptionHandler { _, e -> e.printStackTrace() }
 
+    private var state : State = State.IDLE
+
     fun onChatEvent(chat: ChatContent) {
-        launch {
-            postChat.get(PostChat.Param(chat.teamIndex, chat.roomIndex, chat.chatContent))
+        when {
+            chat.chatContent.startsWith("@등록 ") -> {
+                launch { enterRegisterState(chat.chatContent.substringAfter("@등록 ")) }
+            }
+            state is State.REGISTER -> {
+                launch { getFile(chat) }
+            }
         }
+    }
+
+    private suspend fun enterRegisterState(key: String) {
+        if (key.isEmpty()) { return }
+        state = State.REGISTER(key)
+        postChat("파일을 업로드 해주세요.")
+    }
+
+
+    private suspend fun postChat(content: String) {
+        postChat.get(PostChat.Param(teamIndex, roomIndex, content))
+    }
+
+    private suspend fun getFile(chat: ChatContent) {
+        val key = (state as? State.REGISTER)?.key
+        when {
+            chat.chatFile == null -> {
+                postChat("파일이 없습니다.")
+            }
+            key != null -> {
+                registerFile(
+                    RegisterFile.Param(
+                        chat.teamIndex,
+                        chat.chatFile.id,
+                        chat.chatIndex,
+                        chat.chatFile.name,
+                        (state as State.REGISTER).key
+                    )
+                )
+                postChat("등록 완료")
+            }
+            else -> {
+                /* no-op */
+            }
+        }
+    }
+
+    private suspend fun postFile(filePath: String) {
+
     }
 }
